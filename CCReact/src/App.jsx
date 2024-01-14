8c45a091146298fbfc238d024059863ae7d0cf81
myles-hash <mylesbillington@gmail.com>
2024-01-14 17:32:02 +0000

    final changes

--- CCReact/src/App.jsx
+++ CCReact/src/App.jsx
@@ -1,7 +1,10 @@
 import { useState, useEffect } from "react";
 
 export default function App() {
-  const [cookies, setCookies] = useState(100);
+  const initialCookies = 0;
+  const initialCps = 1;
+  const initialPurchasedItems = [];
+  const [cookies, setCookies] = useState(0);
   const [cps, setCps] = useState(1);
   const [purchasedItems, setPurchasedItems] = useState([]);
   const [secretSpotClicked, setSecretSpotClicked] = useState(false);
@@ -51,10 +54,18 @@ export default function App() {
     }
   }
 
+  function resetGame() {
+    setCookies(initialCookies);
+    setCps(initialCps);
+    setPurchasedItems(initialPurchasedItems);
+    setSecretSpotClicked(false);
+  }
+
+
   return (
     <div className="gameContainer">
       <h1 className="cookieCounter">Cookies : {cookies}</h1>
-      <button onClick={cookiesPlusOne}>{secretSpotClicked ? "+50" : "+1"}</button>
+      <button className="plusClick" onClick={cookiesPlusOne}>{secretSpotClicked ? "+50" : "+1"}</button>
       <p>Cookies per second : {cps}</p>
       <div className="shopContainer">
         <h2>Shop</h2>
@@ -67,9 +78,10 @@ export default function App() {
           </div>
         ))}
       </div>
+      <p className="sSMessage">Look for the secret spot on the page! If you find and click on it you get a <span className="special">special prize! </span></p>
       <div className="secretSpot" onClick={secretSpotClick}>
-        <p>SS spot</p>
       </div>
+      <button onClick={resetGame}>Reset Game</button>
     </div>
   );
 }
